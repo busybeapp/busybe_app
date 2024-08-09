5f414f4e4bb1e03779de8f55da0bb1abb8976c3a
Eli Segal <eli.segal@gmail.com>
2024-08-10 01:04:27 +0300

    feat: Added integration test

---
+++ integration_test/tasks_test.dart
@@ -0,0 +1,15 @@
+import 'package:clearslate_app/main.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('app test', () {
+    testWidgets('show list of tasks', (tester) async {
+      await tester.pumpWidget(const MyApp());
+      expect(find.text('task1'), findsOneWidget);
+    });
+  });
+}
