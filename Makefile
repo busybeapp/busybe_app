c61653190f183095b2c3ca68c212ae0fbf17dac7
Eli Segal <eli.segal@dynamicyield.com>
2024-10-12 00:59:57 +0300

    feat: Allow to configure cloud url

--- Makefile
+++ Makefile
@@ -3,14 +3,14 @@
 integration-tests:
 	flutter drive \
     	--driver=test_driver/integration_test.dart \
-        --target=integration_test/app_test.dart
+        --target=integration_test/app_test.dart \
+        --dart-define="CLOUD_URL=http://localhost:8080"
 
 unit-tests:
 	flutter test
 
 start-wiremock:
 	$(MAKE) stop-wiremock || echo "Wiremock not running"
-	wget -O wiremock/wiremock-standalone.jar https://repo1.maven.org/maven2/org/wiremock/wiremock-standalone/3.9.1/wiremock-standalone-3.9.1.jar
 	java -jar wiremock/wiremock-standalone.jar --root-dir wiremock &
 
 stop-wiremock:
--- lib/entries_repo.dart
+++ lib/entries_repo.dart
@@ -1,11 +1,9 @@
-import 'dart:convert';
-
+import 'package:dio/dio.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
-import 'package:http/http.dart' as http;
 
-part 'entries_repo.g.dart';
 part 'entries_repo.freezed.dart';
+part 'entries_repo.g.dart';
 
 @freezed
 class Entry with _$Entry {
@@ -15,22 +13,23 @@ class Entry with _$Entry {
 }
 
 class EntriesRepository {
-  EntriesRepository(this.client);
+  final Dio dio = Dio();
 
-  final http.Client client;
+  EntriesRepository(String cloudUrl) {
+    dio.options.baseUrl = cloudUrl;
+  }
 
   Future<List<Entry>> getEntries() async {
-    final response = await http.get(Uri.http('localhost:8080', '/api/entries'));
-    final json = jsonDecode(response.body) as Map<String, dynamic>;
-    var list = List<Entry>.from(
-        json["entries"].map((model) => Entry(title: model["title"])));
-    return list;
+    final response = await dio.get("/api/entries");
+    return List<Entry>.from(
+        response.data["entries"].map((model) => Entry(title: model["title"])));
   }
 }
 
 @riverpod
 EntriesRepository entriesRepository(EntriesRepositoryRef ref) {
-  return EntriesRepository(http.Client());
+  return EntriesRepository(const String.fromEnvironment('CLOUD_URL',
+      defaultValue: 'https://clear-slate-8b4de92f5776.herokuapp.com'));
 }
 
 @riverpod
