56bfbe53cace709e398c404961307a993ea787ed
eli segal <eli.segal@dynamicyield.com>
2024-11-05 07:15:45 +0200

    feat: PR review fixes

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,6 +1,7 @@
 import 'package:integration_test/integration_test.dart';
 import './entries_test.dart' as entries_test;
 
+// Aggregation of all component tests
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
 
